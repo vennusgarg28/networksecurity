cc6c79eec8d126bc17cf64aaaf732e101cc44433
venusgarg28 <venusgarg288@gmail.com>
2026-02-13 21:36:46 +0530

    commit

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,13 +25,14 @@ from sklearn.ensemble import (
 )
 import mlflow
 from urllib.parse import urlparse
-
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+dagshub.init(repo_owner='vennusgarg28', repo_name='networksecurity', mlflow=True)
+
+
 '''
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"'''
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/vennusgarg28/networksecurity.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="venusgarg28"
+"'''
 
 
 
@@ -46,7 +47,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-       # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        mlflow.set_registry_uri("https://dagshub.com/vennusgarg28/networksecurity.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
